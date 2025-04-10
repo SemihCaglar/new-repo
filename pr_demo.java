da378f4c58824976635a44671ded4cc417d0fd21
Semih Çağlar <semihcaglar28@gmail.com>
2025-04-10 12:34:34 +0300

    Added pr_test.txt via API

---
+++ pr_demo.java
@@ -0,0 +1,53 @@
+"
+
+public class DemoPR {
+
+    public static void main(String[] args) {
+        // this makes a new scanner, which can read from
+        // STDIN, located at System.in. The scanner lets us look
+        // for tokens, aka stuff the user has entered.
+        Scanner sc = new Scanner(System.in);
+
+        int a = 5; // Setting variable a to 5
+
+        taskTmpPath = ttp; // task tmp
+        
+        // int temp = 0;
+        // System.out.println("Temp: " + temp);
+
+        // TODO: Refactor main method for clarity and error handling.
+        System.out.println("Demo complete.");
+    }
+    
+    /**
+     * Substract two numbers.
+     */
+    public static int substract(int x, int y) {
+        // add x and y
+        return x - y;
+    }
+    
+    public static void beautify() {
+        /* ***********************
+           *    Section Divider    *
+           *********************** */
+        System.out.println("Beautification method executed.");
+    }
+    
+    public static void attributedMethod() {
+        // Added by John Doe
+        System.out.println("Attributed method executed.");
+    }
+    
+    public void setFitnessePort(int fitnessePort) {
+        /* I dedicate all this code, all my work, to my wife,
+        Darlene, who will have to support me and our children
+        and the dog once it gets released into the public.*/
+
+        // Port on which fitnesse would run. Defaults to 8082
+        this.fitnessePort = fitnessePort;
+    }
+}
+
+
+"
